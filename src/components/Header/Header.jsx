5b5943d7843ad3b3c2499fe5e4b830603ea4f4fa
Artem Potapchick <49004382+ArtemPotapchick@users.noreply.github.com>
2020-10-04 00:56:51 +0300

    Create Header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './Header.module.css'
+
+const Header = () => {
+    return (
+        <header className={s.header}>
+        <img src="https://sun9-8.userapi.com/c853620/v853620963/13b4d5/aFszB8VaQCY.jpg" alt="#"></img>
+      </header>
+
+
+    );
+}
+export default Header;
